497c4c154224e9868aa9f4979d14557a487e6231
dev <dev@dev.com>
2023-09-28 01:11:25 +0300

    message gitsss

---
+++ lib/main.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+// ignore: use_key_in_widget_constructors
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.yellow,
+        appBar: AppBar(
+          title: Text(
+            'Baslik',
+            style: TextStyle(
+              color: Colors.red,
+              fontSize: 30.2,
+            ),
+          ),
+          backgroundColor: Colors.amber,
+        ),
+        body: Text('merhaba',
+            style: TextStyle(
+              color: Colors.pink,
+              fontWeight: FontWeight.w500,
+              fontSize: 24,
+            )),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            print('helin tikladi');
+          },
+          child: Icon(
+            Icons.access_alarm,
+            color: Colors.black,
+            size: 45.2,
+            weight: 1.2,
+          ),
+          backgroundColor: Colors.red,
+        ),
+      ),
+    );
+  }
+}
